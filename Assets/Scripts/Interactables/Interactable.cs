b9a22b671bd89e67a4a43f78b1a231d3523c515a
Chirag Agarwal <152566488+indcrediBro@users.noreply.github.com>
2025-10-15 18:06:43 +0530

    Thicker Outlines

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -14,7 +14,7 @@ public abstract class Interactable : MonoBehaviour
         if (CallInteracted) { interacted = new UnityEvent(); }
         if (outline == null) { outline = GetComponent<Outline>(); }
         outline.OutlineMode = Outline.Mode.OutlineVisible;
-        outline.OutlineWidth = 10f;
+        outline.OutlineWidth = 20f;
         outline.OutlineColor = Color.red;
         DisableOutline();
     }
